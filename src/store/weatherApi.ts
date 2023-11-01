18903f03468f8f9587821961a1751b954230a8e6
roman <romulus.hund@gmail.com>
2023-11-01 11:38:00 +0200

    .env has been added

--- src/store/weatherApi.ts
+++ src/store/weatherApi.ts
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import {getTodayDate, getYesterdayDate} from "../helpers";
 
-const apiKey = '5a8ad698f92a46c9be4bd4050c1caec4'
+const apiKey = process.env.REACT_APP_KEY
 
 export const weatherApi = createApi({
     reducerPath: 'weatherApi',
