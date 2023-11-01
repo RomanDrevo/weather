0b1dab792e56aab88c0d4e1b76609d92db464ea4
roman <romulus.hund@gmail.com>
2023-11-01 12:26:55 +0200

    css align

--- src/components/WeatherDetails.tsx
+++ src/components/WeatherDetails.tsx
@@ -34,12 +34,12 @@ const WeatherDetails = ({location}: {location: WeatherData}) => {
             <div className='weather-details'>
                 <div className='weather-details-text temp'>{location?.temp}&deg;</div>
                 <div className='historical-weather-details'>
-                    <span style={{marginRight: 10}} className='weather-details-text'>H:{historicalWeather?.data[0].max_temp}&deg;</span>
+                    <span className='weather-details-text'>H:{historicalWeather?.data[0].max_temp}&deg;</span>
                     <span className='weather-details-text'>L: {historicalWeather?.data[0].min_temp}&deg;</span>
                 </div>
 
                 <div className='location'>
-                    <div style={{marginTop: 10}} className='weather-details-text'>{location?.city_name}, {location.country_code}</div>
+                    <div className='weather-details-text'>{location?.city_name}, {location.country_code}</div>
                     <div className='weather-details-text'>{location?.weather.description}</div>
                 </div>
 
